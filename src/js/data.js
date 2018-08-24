98702b14a138153581eca23e30c3af3622c0597d
Brenda Sánchez Reyes <sanrey254@hotmail.com>
2018-08-24 17:35:12 -0500

    Cambiando framework css a materialize

--- src/js/data.js
+++ src/js/data.js
@@ -1,3 +1,4 @@
+
 window.getData = (search, page) => {
   fetch(`https://www.omdbapi.com/?apikey=add73b1a&s=${search}&page=${page}`)
     .then(response => response.json())
@@ -6,19 +7,21 @@ window.getData = (search, page) => {
     })
     .catch(() => {
       console.log('Error');
+      document.getElementById('moviesGrid').innerHTML = '';
       document.getElementById('dataGrid').innerHTML = '<div class="mt-5 px-3 px-md-0 mx-auto"><img src="img/no-result.png"class="img-fluid"></div>';
     });
 };
 
-document.getElementById('search-string-button').addEventListener('click', event => {
+/* document.getElementById('search-string-button').addEventListener('click', event => {
   event.preventDefault();
   let searchString = document.getElementById('search-string').value;
   let newSearchString = searchString.replace(' ', '+');
   getData(newSearchString, 1);
   document.getElementById('search-string').value = '';
-});
+}); */
 
 window.drawData = (dataArray) => {
+  
   let result = '';
   let poster = '';
   // console.log(typeof(dataArray));
@@ -29,28 +32,32 @@ window.drawData = (dataArray) => {
     } else {
       poster = data.Poster;
     }
-    result += `<div class="col-6 col-sm-4 col-md-3 col-lg-3 my-3">
-					    <div class="card hoverable">
-					    <img class="img-fluid height-img" src="${poster}"><div class="card-body">
-                <h4 class="card-title">${data.Title}</h4>
-                <div class="row">
-                <div class="col-md-6"><p class="card-text">${type}</p></div>
-                <div class="col-md-6"><span class="badge badge-dark"><i class="fas fa-info-circle"></i><button class="no-btn text-white" onclick="getDetails('${data.imdbID}')">Detalles</button></span></div>
-                </div></div></div></div>`;
+    result += `<div class="col offset-s1 s10 m6 l3"><div class="card">
+          <div class="card-image">
+            <img class="height-img" src="${poster}">
+            <a class="btn-floating halfway-fab waves-effect waves-light red" onclick="getDetails('${data.imdbID}')"><i class="material-icons">speaker_notes</i></a>
+          </div>
+          <div class="card-content">
+            <p><b>${data.Title}<b></p>
+          </div>
+          <div class="card-action">
+            ${type}
+          </div></div></div>`;
   });
+  document.getElementById('moviesGrid').innerHTML = '';
   document.getElementById('dataGrid').innerHTML = result;
 };
 
 window.getTypeBage = (type) => {
   let result = '';
   if (type === 'movie') {
-    result = '<span class="badge badge-warning"><i class="fas fa-film"></i> Película</span>';
+    result = '<a><span class="badge amber white-text lowerCase"><i class="fas fa-film"></i> Movie</span></a>';
   }
   if (type === 'series') {
-    result = '<span class="badge badge-success"><i class="fas fa-tv"></i> Serie</span>';
+    result = '<a><span class="badge red darken-4 white-text lowerCase"><i class="fas fa-tv"></i> Serie</span></a>';
   }
   if (type === 'game') {
-    result = '<span class="badge badge-info"><i class="fas fa-gamepad"></i> Videojuego</span>';
+    result = '<a><span class="badge green accent-4 white-text lowerCase"><i class="fas fa-gamepad"></i> Videogame</span></a>';
   }
   return result;
 };
@@ -73,16 +80,23 @@ window.drawDataDetails = (dataArray) => {
   } else {
     poster = dataArray.Poster;
   }
-  const card = `<div class="container pl-4"><div class="card"><div class="row ">
-        <div class="col-md-6 px-0"><img src="${poster}" class="img-fluid img-card"></div>
-          <div class="col-md-6 pl-0"><div class="card-block p-3 text">
-              <h4 class="card-title-details text-center">${dataArray.Title}</h4>
-              <div class="text-left">
-                <span class="card-text-details pb-1 d-block"><strong>Clasificación: </strong>${dataArray.Rated}</span><span class="card-text-details py-1 d-block"><strong>Año: </strong>${dataArray.Year}</span><span class="card-text-details py-1 d-block"><strong>País: </strong>${dataArray.Country}</span><span class="card-text-details py-1 d-block"><strong>Categoríass: </strong>${dataArray.Genre}</span><span class="card-text-details pt-1 d-block small-text"><strong>Reseña: </strong>${dataArray.Plot}</span>
-            </div></div></div></div></div></div></div>`;
+  const card = `<div class="row">
+  <div class=""><h4>${dataArray.Title}</h4></div>
+  <div class="col s5">
+  <div class="mt-4"><img class="responsive-img" src="${poster}"></div>
+  </div>
+  <div class="col s7">
+  <div class="card-action-text">
+      <p><strong>Rated: </strong>${dataArray.Rated}</span></p>
+      <p><strong>Year: </strong>${dataArray.Year}</span></p>
+      <p><strong>Country: </strong>${dataArray.Country}</span></p>
+      <p><strong>Genre: </strong>${dataArray.Genre}</span></p>
+      <p><strong>Plot: </strong>${dataArray.Plot}</span></p>
+  </div></div></div>`;
   swal({
     html: card,
     confirmButtonText: 'Cerrar',
+    confirmButtonColor: '#F44336',
     backdrop: 'rgba(24, 24, 36, 0.92)'
   });
 };
@@ -101,14 +115,18 @@ window.getMoviesTop = () => {
         } else {
           poster = data.Poster;
         }
-        result += `<div class="col-6 col-sm-4 col-md-3 col-lg-3 my-3"><div class="card hoverable">
-					    <img class="img-fluid height-img" src="${poster}">
-					    <div class="card-body"><h4 class="card-title">${data.Title}</h4>
-                <div class="row">
-                  <div class="col-md-6"><p class="card-text">${type}</p></div>
-                    <div class="col-md-6"><span class="badge badge-dark"><i class="fas fa-info-circle"></i><button class="no-btn text-white" onclick="getDetails('${data.imdbID}')">Detalles</button></span></div>
-                </div></div></div></div>`;
-        document.getElementById('dataGrid').innerHTML = result;
+        result += `<div class="col offset-s1 s10 m6 l3"><div class="card">
+          <div class="card-image">
+            <img class="height-img" src="${poster}">
+            <a class="btn-floating halfway-fab waves-effect waves-light red" onclick="getDetails('${data.imdbID}')"><i class="material-icons">speaker_notes</i></a>
+          </div>
+          <div class="card-content">
+            <p><b>${data.Title}<b></p>
+          </div>
+          <div class="card-action">
+            ${type}
+          </div></div></div>`;
+        document.getElementById('moviesGrid').innerHTML = result;
       })
       .catch(error => {
         console.log('Error', error);
